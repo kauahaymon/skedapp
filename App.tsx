1e6481016a7fe39cc9fe1e503225f97c059d2a1f
Kauã Haymon <kauahaymon@hotmail.com>
2024-11-22 19:56:37 -0400

    autenticação com o google

--- App.tsx
+++ App.tsx
@@ -1,19 +1,24 @@
-import { 
-  SafeAreaView, 
-  StyleSheet, 
-} from 'react-native';
-
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import Routes from './src/screens/routes';
+import {
+  SafeAreaView,
+  StyleSheet,
+} from 'react-native'
 
+import { StatusBar } from 'expo-status-bar'
+import { ClerkProvider } from '@clerk/clerk-expo'
+import { tokenCashe } from './src/storage/tokenCache';
+import Routes from './src/screens/routes'
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container} >
-      <StatusBar style='light' />
-      <Routes />
-    </SafeAreaView>
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCashe}>
+      <SafeAreaView style={styles.container} >
+        <StatusBar style='light' />
+        <Routes />
+      </SafeAreaView>
+    </ClerkProvider>
   )
 }
 
